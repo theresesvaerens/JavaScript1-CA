c0cc327a2b32b52508ea97b5481ff3f10a1ca4c3
theresesvaeren <thesva02455@stud.noroff.no>
2025-04-04 09:14:03 +0200

    js for navbar on mobile

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
     const bar = document.getElementById("bar");
+    const close = document.getElementById("close");
     const nav = document.getElementById("navbar");
 
-    if(bar){
+    if (bar) {
         bar.addEventListener("click", () => {
             nav.classList.add("active");
+        });
+    }
+
+    if (close) { // Må være inni DOMContentLoaded
+        close.addEventListener("click", () => {
+            nav.classList.remove("active");
         });
     }
 });
