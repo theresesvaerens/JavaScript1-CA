fb48738087528a39b0da04aba1eeb42d2fadc1cf
theresesvaeren <thesva02455@stud.noroff.no>
2025-04-06 10:01:15 +0200

    removed individual product pages. Added one produtct page, html, css

---
+++ products.js
@@ -0,0 +1,98 @@
+const products = [
+    {
+      id: 1,
+      title: "Hobbs & Shaw",
+      price: 169,
+      description: "Lawman Luke Hobbs and outcast Deckard Shaw form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.",
+      genre: "Action",
+      released: 2019,
+      ageRating: "12+",
+      duration: "2h 16m",
+      rating: "6,5/10",
+      image: "Images/Square_Eyes_Cover1.jpeg"
+    },
+    {
+      id: 2,
+      title: "Godzilla: King of the Monsters",
+      price: 169,
+      description: "As ancient giant monsters awaken, the cryptozoological agency Monarch faces Godzilla, Mothra, Rodan and King Ghidorah, all vying for supremacy.",
+      genre: "Action",
+      released: 2019,
+      ageRating: "12+",
+      duration: "2h 12m",
+      rating: "6,0/10",
+      image: "Images/Square_Eyes_Cover2.jpeg"
+    },
+    {
+      id: 3,
+      title: "Once Upon a Time In Hollywood",
+      price: 169,
+      description: "A faded TV star and his stuntman strive to achieve fame and success in Hollywood. The year is 1969, the golden age is coming to an end and the Manson sect is wreaking havoc in Los Angeles.",
+      genre: "Comedy",
+      released: 2019,
+      ageRating: "15+",
+      duration: "2h 40m",
+      rating: "7,6/10",
+      image: "Images/Square_Eyes_Cover3.jpeg"
+    },
+    {
+      id: 4,
+      title: "Scream",
+      price: 149,
+      description: "25 years after brutal murders shocked the quiet small town of Woodsboro, a new killer has found the Ghostface mask and singled out a group of teenagers.",
+      genre: "Horror",
+      released: 2022,
+      ageRating: "15+",
+      duration: "1h 54m",
+      rating: "6,3/10",
+      image: "Images/Square_Eyes_Cover4.jpeg"
+    },
+    {
+      id: 5,
+      title: "Sweetheart, O Mistério da Ilha",
+      price: 149,
+      description: "A woman washes ashore a small uninhabited island after an accident. The situation turns from bad to worse when she realizes there is a mysterious creature hunting at night.",
+      genre: "Horror",
+      released: 2019,
+      ageRating: "15+",
+      duration: "1h 22m",
+      rating: "5,8/10",
+      image: "Images/Square_Eyes_Cover5.jpeg"
+    },
+    {
+      id: 6,
+      title: "The Addams Family",
+      price: 149,
+      description: "A greedy home improvement expert moves into the Addams family's neighborhood. She expresses that the family's spooky house is bad for business, and it soon becomes obvious that the peaceful life of the Addams family is about to end.",
+      genre: "Children",
+      released: 2019,
+      ageRating: "6+",
+      duration: "1h 27m",
+      rating: "5,8/10",
+      image: "Images/Square_Eyes_Cover6.jpeg"
+    },
+    {
+      id: 7,
+      title: "The Batman",
+      price: 169,
+      description: "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.",
+      genre: "Action",
+      released: 2022,
+      ageRating: "15+",
+      duration: "2h 56m",
+      rating: "7,8/10",
+      image: "Images/Square_Eyes_Cover7.jpeg"
+    },
+    {
+      id: 8,
+      title: "Toy Story 4",
+      price: 149,
+      description: "When a new toy called \"Forky\" joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy.",
+      genre: "Children",
+      released: 2019,
+      ageRating: "6+",
+      duration: "1h 40m",
+      rating: "7,7/10",
+      image: "Images/Square_Eyes_Cover8.jpeg"
+    }
+  ];
--- script.js
+++ script.js
@@ -1,178 +1,33 @@
-document.addEventListener("DOMContentLoaded", () => {
-    
-    const cart = [];
-    let cartItemCount = 0;
-    const cartCount = document.querySelector('.cart-item-count');
-    const cartTotal = document.querySelector('.total-price');
-    
-    
-    const bar = document.getElementById("bar");
-    const close = document.getElementById("close");
-    const nav = document.getElementById("navbar");
+const productContainer = document.querySelector(".product-list");
 
-    if (bar) {
-        bar.addEventListener("click", () => {
-            nav.classList.add("active");
-        });
-    }
+if (productContainer) {
+    displayProducts ();
+}
 
-    if (close) {
-        close.addEventListener("click", () => {
-            nav.classList.remove("active");
-        });
-    }
-
-    
-    const cartIcon = document.getElementById("cart");
-    const cartElement = document.querySelector(".cart");
-    const cartClose = document.querySelector("#cart-close");
-
-    cartIcon.addEventListener("click", () => cartElement.classList.add("active"));
-    cartClose.addEventListener("click", () => cartElement.classList.remove("active"));
-
-    
-    const addCartButtons = document.querySelectorAll(".add-cart");
-    const cartContent = document.querySelector(".cart-content");
-
-    addCartButtons.forEach(button => {
-        button.addEventListener("click", event => {
-            const productBox = event.target.closest(".product-box");
-            addToCartFromList(productBox);
-        });
-    });
-
-    
-    const addToCartFromList = productBox => {
-        const productImgScr = productBox.querySelector("img").src;
-        const productTitle = productBox.querySelector(".product-title").textContent;
-        const productPrice = productBox.querySelector(".price").textContent;
-
-        
-        const cartItems = cartContent.querySelectorAll(".cart-product-title");
-        for (let item of cartItems) {
-            if (item.textContent === productTitle) {
-                alert("This item is already in the cart.");
-                return;
-            }
-        }
-
-        const cartBox = document.createElement("div");
-        cartBox.classList.add("cart-box");
-        cartBox.innerHTML = `
-            <img src="${productImgScr}" class="cart-img">
-            <div class="cart-detail">
-                <h4 class="cart-product-title">${productTitle}</h4>
-                <span class="cart-price">${productPrice}</span>
-                <div class="cart-quantity">
-                    <button class="decrement">-</button>
-                    <span class="number">1</span>
-                    <button class="increment">+</button>
+function displayProducts() {
+    products.forEach(product =>{
+        const productCard = document.createElement("div");
+        productCard.classList.add("productCard");
+        productCard.innerHTML = `
+            <a href="product${product.id}.html"> 
+                <div class="img-box">
+                    <img src="${product.image}" alt="${product.title}">
                 </div>
+                <h2 class="product-title">${product.title}</h2>
+            </a>
+            <div class="price-and-cart">
+                <span class="price">${product.price} NOK</span>
+                <i class="fa-solid fa-cart-shopping add-cart"></i>
             </div>
-            <i class="fa-solid fa-trash cart-remove"></i>
         `;
 
-        cartContent.appendChild(cartBox);
+        productContainer.appendChild(productCard);
 
-        cartBox.querySelector(".cart-remove").addEventListener("click", () => {
-            cartBox.remove();
-            updateCartCount(-1);
-            updateTotalPrice();
+        const imgBox = productCard.querySelector(".img-box");
+        imgBox.addEventListener("click", () => {
+            sessionStorage.setItem("selectedProduct", JSON.stringify(product));
+            window.location.href = "product.html";
         });
-
-        const quantityElement = cartBox.querySelector(".number");
-        const decrementBtn = cartBox.querySelector(".decrement");
-        const incrementBtn = cartBox.querySelector(".increment");
-
-        decrementBtn.addEventListener("click", () => {
-            let quantity = parseInt(quantityElement.textContent);
-            if (quantity > 1) {
-                quantity--;
-                quantityElement.textContent = quantity;
-                updateTotalPrice();
-            }
-        });
-
-        incrementBtn.addEventListener("click", () => {
-            let quantity = parseInt(quantityElement.textContent);
-            quantity++;
-            quantityElement.textContent = quantity;
-            updateTotalPrice();
-        });
-
-        updateCartCount(1);
-        updateTotalPrice();
-    };
-
-    
-    const updateTotalPrice = () => {
-        const totalPriceElement = document.querySelector(".total-price");
-        const cartBoxes = cartContent.querySelectorAll(".cart-box");
-        let total = 0;
-
-        cartBoxes.forEach(cartBox => {
-            const priceElement = cartBox.querySelector(".cart-price");
-            const quantityElement = cartBox.querySelector(".number");
-
-            const price = parseFloat(priceElement.textContent.replace("NOK", "").trim());
-            const quantity = parseInt(quantityElement.textContent);
-            total += price * quantity;
-        });
-
-        totalPriceElement.textContent = `${total.toFixed(2)} NOK`;
-    };
-
-   
-    const updateCartCount = change => {
-        cartItemCount += change;
-
-        if (cartItemCount > 0) {
-            cartCount.style.display = "flex";
-            cartCount.textContent = cartItemCount;
-        } else {
-            cartCount.style.display = "none";
-            cartCount.textContent = "";
-        }
-    };
-
-    
-    const singleAddToCartBtn = document.getElementById('add-to-cart-btn');
-    const productPrice = document.getElementById('single-product-price').innerText;
-
-    const addSingleProductToCart = () => {
-        console.log(typeof cart); 
-        console.log(cart); 
-
-        const product = {
-            title: document.querySelector('.single-pro-title').innerText,
-            price: productPrice.replace('NOK', '').trim(),
-            image: document.querySelector('.single-pro-image img').src
-        };
-
-        
-        if (Array.isArray(cart)) {
-            cart.push(product);
-            updateCart();
-        } else {
-            console.error('Cart is not an array!');
-        }
-    };
-
-    if (singleAddToCartBtn) {
-        singleAddToCartBtn.addEventListener('click', addSingleProductToCart);
-    }
-
-    
-    const updateCart = () => {
-        cartCount.textContent = cart.length;
-
-        let total = 0;
-        cart.forEach(item => {
-            total += parseFloat(item.price);
-        });
-
-        cartTotal.textContent = total.toFixed(2) + ' NOK';
-    };
-});
-
+    });
+}
 
