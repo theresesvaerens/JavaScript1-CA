c86ea699993d677f0677a04fc77f644e6a0c0d4e
theresesvaeren <thesva02455@stud.noroff.no>
2025-04-06 14:01:59 +0200

    Navbar for mobile.

--- script.js
+++ script.js
@@ -219,4 +219,20 @@ if (categoryToggle && categoryList) {
         });
     });
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const bar = document.getElementById("bar");
+    const nav = document.getElementById("navbar");
+    const close = document.getElementById("close");
 
+    if(bar){
+        bar.addEventListener("click", () => {
+            nav.classList.add("active");
+        });
+    }
+
+    if (close) {
+        close.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
+    }
+});
