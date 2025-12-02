d10c77eb81137f39a54565a9253874b728fd83f1
theresesvaeren <thesva02455@stud.noroff.no>
2025-12-02 21:09:56 +0100

    Fixed single product page.

--- products.js
+++ products.js
@@ -26,3 +26,53 @@ async function loadFeaturedMovies() {
 }
 
 loadFeaturedMovies();
+
+
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+const imgElem = document.getElementById("product-image");
+const titleElem = document.getElementById("product-title");
+const genreElem = document.getElementById("product-genre");
+const ratingElem = document.getElementById("product-rating");
+const descriptionElem = document.getElementById("product-description");
+const priceElem = document.getElementById("product-price");
+const addToCartBtn = document.getElementById("add-to-cart-btn");
+
+
+async function loadProduct() {
+    try {
+        const res = await fetch(`https://v2.api.noroff.dev/square-eyes/${id}`);
+        const data = await res.json();
+        const p = data.data;
+
+
+        imgElem.src = p.image.url;
+        imgElem.alt = p.title;
+
+        titleElem.textContent = p.title;
+        genreElem.textContent = `Genre: ${p.genre}`;
+        ratingElem.textContent = `Rating: ${p.rating}/10`;
+
+        descriptionElem.textContent = p.description;
+        priceElem.textContent = p.price;
+
+    
+        addToCartBtn.addEventListener("click", () => {
+            addToCart({
+                id: p.id,
+                title: p.title,
+                price: p.price,
+                image: p.image.url
+            });
+
+            addToCartBtn.textContent = "Added!";
+            addToCartBtn.style.background = "#4CAF50";
+        });
+
+    } catch (error) {
+        console.error("Error loading product:", error);
+    }
+}
+
+loadProduct();
