3ee2b7cffe46edd9ee933aebf1b836e63c3475d3
theresesvaeren <thesva02455@stud.noroff.no>
2025-12-02 17:14:32 +0100

    Fetched products from API, fixed styling index page.

--- products.js
+++ products.js
@@ -1,98 +1,28 @@
-const products = [
-    {
-      id: 1,
-      title: "Hobbs & Shaw",
-      price: 169,
-      description: "Lawman Luke Hobbs and outcast Deckard Shaw form an unlikely alliance when a cyber-genetically enhanced villain threatens the future of humanity.",
-      genre: "action",
-      released: 2019,
-      ageRating: "12+",
-      duration: "2h 16m",
-      rating: "6,5/10",
-      image: "Images/Square_Eyes_Cover1.jpeg"
-    },
-    {
-      id: 2,
-      title: "Godzilla: King of the Monsters",
-      price: 169,
-      description: "As ancient giant monsters awaken, the cryptozoological agency Monarch faces Godzilla, Mothra, Rodan and King Ghidorah, all vying for supremacy.",
-      genre: "action",
-      released: 2019,
-      ageRating: "12+",
-      duration: "2h 12m",
-      rating: "6,0/10",
-      image: "Images/Square_Eyes_Cover2.jpeg"
-    },
-    {
-      id: 3,
-      title: "Once Upon a Time In Hollywood",
-      price: 169,
-      description: "A faded TV star and his stuntman strive to achieve fame and success in Hollywood. The year is 1969, the golden age is coming to an end and the Manson sect is wreaking havoc in Los Angeles.",
-      genre: "comedy",
-      released: 2019,
-      ageRating: "15+",
-      duration: "2h 40m",
-      rating: "7,6/10",
-      image: "Images/Square_Eyes_Cover3.jpeg"
-    },
-    {
-      id: 4,
-      title: "Scream",
-      price: 149,
-      description: "25 years after brutal murders shocked the quiet small town of Woodsboro, a new killer has found the Ghostface mask and singled out a group of teenagers.",
-      genre: "horror",
-      released: 2022,
-      ageRating: "15+",
-      duration: "1h 54m",
-      rating: "6,3/10",
-      image: "Images/Square_Eyes_Cover4.jpeg"
-    },
-    {
-      id: 5,
-      title: "Sweetheart, O Mistério da Ilha",
-      price: 149,
-      description: "A woman washes ashore a small uninhabited island after an accident. The situation turns from bad to worse when she realizes there is a mysterious creature hunting at night.",
-      genre: "horror",
-      released: 2019,
-      ageRating: "15+",
-      duration: "1h 22m",
-      rating: "5,8/10",
-      image: "Images/Square_Eyes_Cover5.jpeg"
-    },
-    {
-      id: 6,
-      title: "The Addams Family",
-      price: 149,
-      description: "A greedy home improvement expert moves into the Addams family's neighborhood. She expresses that the family's spooky house is bad for business, and it soon becomes obvious that the peaceful life of the Addams family is about to end.",
-      genre: "children",
-      released: 2019,
-      ageRating: "6+",
-      duration: "1h 27m",
-      rating: "5,8/10",
-      image: "Images/Square_Eyes_Cover6.jpeg"
-    },
-    {
-      id: 7,
-      title: "The Batman",
-      price: 169,
-      description: "When a sadistic serial killer begins murdering key political figures in Gotham, Batman is forced to investigate the city's hidden corruption and question his family's involvement.",
-      genre: "action",
-      released: 2022,
-      ageRating: "15+",
-      duration: "2h 56m",
-      rating: "7,8/10",
-      image: "Images/Square_Eyes_Cover7.jpeg"
-    },
-    {
-      id: 8,
-      title: "Toy Story 4",
-      price: 149,
-      description: "When a new toy called \"Forky\" joins Woody and the gang, a road trip alongside old and new friends reveals how big the world can be for a toy.",
-      genre: "children",
-      released: 2019,
-      ageRating: "6+",
-      duration: "1h 40m",
-      rating: "7,7/10",
-      image: "Images/Square_Eyes_Cover8.jpeg"
-    }
-  ];
+async function loadFeaturedMovies() {
+  const res = await fetch("https://v2.api.noroff.dev/square-eyes");
+  const data = await res.json();
+  const products = data.data;
+
+  const container = document.querySelector(".product-list");
+
+  products.forEach(product => {
+      const item = document.createElement("div");
+      item.classList.add("product-item");
+
+      item.innerHTML = `
+          <a href="product.html?id=${product.id}">
+              <div class="img-box">
+                  <img src="${product.image.url}" alt="${product.title}">
+              </div>
+              <div class="info">
+                  <h2 class="title">${product.title}</h2>
+                  <p class="price">${product.price} NOK</p>
+              </div>
+          </a>
+      `;
+
+      container.appendChild(item);
+  });
+}
+
+loadFeaturedMovies();
